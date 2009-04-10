4fc8a40920ad447585a2fbc1ffbecc6e949572be
Abigail <abigail@abigail.be>
2009-04-10 16:26:42 +0200

    Upped version

--- lib/Test/Regexp.pm
+++ lib/Test/Regexp.pm
@@ -12,7 +12,7 @@ use Test::Builder;
 our @EXPORT  = qw [match no_match];
 our @ISA     = qw [Exporter Test::More];
 
-our $VERSION = '2009040901';
+our $VERSION = '2009041001';
 
 BEGIN {
     binmode STDOUT, ":utf8";
