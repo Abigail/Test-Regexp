a73da31595ed3dc3384fbd20b43144a52d88de93
Abigail <abigail@abigail.be>
2009-04-14 17:03:18 +0200

    Removed duplicate entry in MANIFEST

--- lib/Test/Regexp.pm
+++ lib/Test/Regexp.pm
@@ -12,7 +12,7 @@ use Test::Builder;
 our @EXPORT  = qw [match no_match];
 our @ISA     = qw [Exporter Test::More];
 
-our $VERSION = '2009041002';
+our $VERSION = '2009041401';
 
 BEGIN {
     binmode STDOUT, ":utf8";
