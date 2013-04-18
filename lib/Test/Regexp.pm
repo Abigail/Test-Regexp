1a5858bb89041424c94b85fe4f682c8953e8766f
Abigail <abigail@abigail.be>
2013-04-18 19:32:31 +0200

    Made a 'name' accessor for Test::Regexp::Object

--- lib/Test/Regexp.pm
+++ lib/Test/Regexp.pm
@@ -12,7 +12,7 @@ use Test::Builder;
 our @EXPORT  = qw [match no_match];
 our @ISA     = qw [Exporter Test::More];
 
-our $VERSION = '2013041501';
+our $VERSION = '2013041801';
 
 BEGIN {
     binmode STDOUT, ":utf8";
@@ -526,6 +526,7 @@ sub no_match {
                            @_;
 }
 
+sub name {$name {+shift}}
 
 
 
