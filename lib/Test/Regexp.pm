85f19564cdc6826d253a7e65a32ea0d0c0e1a7b9
Abigail <abigail@abigail.be>
2016-06-05 00:45:49 +0200

    Upped version

--- lib/Test/Regexp.pm
+++ lib/Test/Regexp.pm
@@ -17,7 +17,7 @@ use Test::Builder;
 our @EXPORT  = qw [match no_match];
 our @ISA     = qw [Exporter Test::More];
 
-our $VERSION = '2016052701';
+our $VERSION = '2016060501';
 
 
 my $Test = Test::Builder -> new;
